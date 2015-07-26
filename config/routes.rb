253971578eaf595c73514238b81523b5ae3da32d
Georgy <geovo851@gmail.com>
2015-07-26 07:22:42 -0700

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'articles#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
